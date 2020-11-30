8c722a74bae905bf43951ac633abdfa8c4fd3497
Sourabh Mahato <sourabhmahato5701@gmail.com>
2020-11-30 19:27:17 +0530

    feat: added pubg quiz

--- index.js
+++ index.js
@@ -1,50 +1,77 @@
-var readlineSync = require ("readline-sync");
-var score =0;
-var userName = readlineSync.question("What's your name? ");
-console.log("WELCOME TO DO YOU KNOW SOURABH, " +userName);
+var readlineSync = require("readline-sync");
+const chalk = require('chalk');
+var score = 0;
+console.log (chalk.blue("WELCOME TO THE PUBG QUIZ"));
+console.log("--------------------------");
+var userName = readlineSync.question("What's your name?\n");
+console.log("Let's start the quiz, " + userName);
 
-//data of high score
-var highScores =[{
-  name: "Tanay",
-  score: 3,
-},
-{
-  name: "Akansha",
-  score: 2,
-},
-];
-
-//function of the app
-function play (question, answer){
-  var userAnswer = readlineSync.question(question);
-  if (userAnswer.toUpperCase() == answer.toUpperCase()){
-    console.log("sahi jawab...");
-    score = score +1;
-  } else {
-    console.log("galat jawab...");
-  }
-  console.log("your score is: ",score);
-    console.log("--------------");
-}
-//array of objects creation
-var questions = [{
-  question: "where do sourabh live? \n" ,
-  answer: "Puruliya",
+var highScores = [{
+  name: "Sourabh",
+  score: 5,
 },{
-  question: "his favorite superhero? \n",
-  answer: "Dhruv"
-},{question: "where do he work? \n",
-    answer: "Microsoft"
+  name: "Gourab",
+  score: 4
 }];
 
-// for loop
-for (var i=0; i<questions.length; i++){
-  var currentQuestion = questions[i];
-  play(currentQuestion.question, currentQuestion.answer)
+//function part 
+function pubg(question, answer){
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer.toUpperCase()==answer.toUpperCase()){
+    score = score + 1 ;
+    console.log(chalk.green("Keep it up, Right Answer"));
+  } else {
+    score = score - 1;
+    console.log(chalk.red("Oops, wrong answer"));
+  }
+   console.log("your score is: ",score);
+    console.log("--------------");
 }
-console.log("congrats, you scored: ",score);
- console.log("--------------");
-console.log("CheckOut the Highscores:");
+var setOfQuestions = [{
+  question: `1. Engine used to develop pubg mobile? 
+  a. Unity
+  b. Unreal
+  c. GameMaker
+  d. GDevelop\n`,
+  answer: "b",
+},{
+  question: `2. Most Powerful sniper?
+  a. AWM
+  b. Kar98K
+  c. M24\n`,
+  answer: "a",
+},{
+  question: `3. Most Powerful Designated marksman rifle (DMR)?
+  a. SLR
+  b. Win94
+  c. Mini14
+  d. SKS\n`,
+  answer: "a",
+
+},{ 
+ question: `4. Highest damage assault rifle?
+  a. M4
+  b. Berryl
+  c. AKM
+  d. Scarl\n`,
+  answer: "c",
+
+},{
+  question: `5. Which weapon has capacity of 100 ammo?
+  a. M4
+  b. M249
+  c. UZI
+  d. Vector\n`,
+  answer: "b",
+
+}];
+
+for (var i = 0; i<setOfQuestions.length; i++){
+  pubg(setOfQuestions[i].question, setOfQuestions[i].answer)
+}
+console.log("WOW, you scored: ", score);
+console.log("--------------");
+console.log(chalk.yellow("CheckOut the Highscores:"));
 for (var j=0; j<highScores.length; j++){
 console.log(highScores[j].name, highScores[j].score);
 }
